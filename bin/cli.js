6639a634679bba763dedb77fd058cfd900fcf24f
fujuewen <fujuewen@yy.com>
2019-01-10 20:58:04 +0800

    Fix

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
-require('v8-compile-cache')
-require('./main')
+const log = require('../lib/utils/log')
+
+if (process.version.match(/v(\d+)\./)[1] < 8) {
+  log.error('fev: Node v8 or greater is required. `fev` did not run.')
+} else {
+  require('v8-compile-cache')
+  require('./main')
+}
+
